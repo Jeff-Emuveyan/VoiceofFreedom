5ae1d9fc113173dc01173d03d7594728b5409634
jeffemuveyan <jemuveyan@seamfix.com>
2020-05-26 13:38:19 +0100

    Added give fragment

---
+++ app/src/main/java/com/bellogate/voiceoffreedom/ui/give/GiveFragment.kt
@@ -0,0 +1,32 @@
+package com.bellogate.voiceoffreedom.ui.give
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bellogate.voiceoffreedom.R
+
+class GiveFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = GiveFragment()
+    }
+
+    private lateinit var viewModel: GiveViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.give_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(GiveViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}

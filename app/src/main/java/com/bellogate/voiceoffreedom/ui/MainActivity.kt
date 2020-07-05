41363f6af0c6919045f08f14fbc2aadea237fd5a
jeffemuveyan <jemuveyan@seamfix.com>
2020-07-05 15:02:57 +0100

    Added About Fragment

--- app/src/main/java/com/bellogate/voiceoffreedom/ui/MainActivity.kt
+++ app/src/main/java/com/bellogate/voiceoffreedom/ui/MainActivity.kt
@@ -55,7 +55,8 @@ class MainActivity : AppCompatActivity() {
             R.id.nav_home,
             R.id.nav_give,
             R.id.nav_devotional,
-            R.id.nav_video
+            R.id.nav_video,
+            R.id.nav_about
         ), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)

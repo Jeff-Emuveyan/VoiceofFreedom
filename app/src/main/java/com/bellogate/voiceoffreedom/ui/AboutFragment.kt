e4b4477540fe6d794708de68c060fa1719452124
jeffemuveyan <jemuveyan@seamfix.com>
2020-07-31 14:50:35 +0100

    About Fragment UI fixes

--- app/src/main/java/com/bellogate/voiceoffreedom/ui/AboutFragment.kt
+++ app/src/main/java/com/bellogate/voiceoffreedom/ui/AboutFragment.kt
@@ -1,11 +1,13 @@
 package com.bellogate.voiceoffreedom.ui
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bellogate.voiceoffreedom.R
+import kotlinx.android.synthetic.main.fragment_about.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,6 +38,13 @@ class AboutFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textView12.movementMethod =  ScrollingMovementMethod();
     }
 
     companion object {
